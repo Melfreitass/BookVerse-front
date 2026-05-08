2b3f9e70658f2fe76d0c1b44e839f5ae2600ec3a
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-05-08 09:51:16 -0300

    feat: edita header

--- BookVerse/src/components/Header/Header.jsx
+++ BookVerse/src/components/Header/Header.jsx
@@ -1,19 +1,48 @@
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.titulo}>
-                <p>BOOKVERSE</p>
+                <NavLink to="/Home" className={styles.atitulo}>
+                    BOOKVERSE
+                </NavLink>
             </div>
 
             <div className={styles.nav}>
                 <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/biblioteca">Biblioteca</Link>
-                    <Link to="/simulados">Simulados</Link>
-                    <Link to="/videos">Videos</Link>
+                    <NavLink
+                        to="/sobre"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.anav} ${styles.active}` : styles.anav
+                        }>
+                        Sobre
+                    </NavLink>
+
+                    <NavLink
+                        to="/biblioteca"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.anav} ${styles.active}` : styles.anav
+                        }>
+                        Biblioteca
+                    </NavLink>
+
+                    <NavLink
+                        to="/simulados"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.anav} ${styles.active}` : styles.anav
+                        }>
+                        Simulados
+                    </NavLink>
+
+                    <NavLink
+                        to="/dicas"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.anav} ${styles.active}` : styles.anav
+                        }>
+                        Dicas
+                    </NavLink>
                 </nav>
             </div>
 
