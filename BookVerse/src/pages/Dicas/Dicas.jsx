44fb0b3b01b5da974d08764c0ff8dd25b99aca42
luana <luana.guerreiro@aluno.senai.br>
2026-05-08 11:31:28 -0300

    feat: adiciona cards de dicas para vestibular

--- BookVerse/src/pages/Dicas/Dicas.jsx
+++ BookVerse/src/pages/Dicas/Dicas.jsx
@@ -20,6 +20,33 @@ function Dicas() {
                     <img src={livro} alt="Livro" />
                 </div>
             </main>
+
+            <section className={styles.cardsSection}>
+                <div className={styles.info}>
+                    <h3>Interpretação da Equipe</h3>
+
+                    <p>Análises detalhadas sobre as tendências dos grandes vestibulares.</p>
+                </div>
+
+                <div className={styles.cards}>
+                    <div className={styles.cardGrande}>
+                        <span>DICAS</span>
+
+                        <h2>💕</h2>
+
+                        <button>Ler Análise</button>
+                    </div>
+
+                    <div className={styles.cardPequeno}>
+                        <h3>💕</h3>
+                    </div>
+
+                    <div className={styles.cardBaixo}>
+                        <span>Atualidade</span>
+                        <h3>💕</h3>
+                    </div>
+                </div>
+            </section>
         </section>
     );
 }
