a2fc1522208d80fa49eb33e7fbdb1fbf85af40e3
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-05-22 08:43:19 -0300

    faet: cria card de personagem

---
+++ BookVerse/src/components/CardPersonagem/CardPersonagem.jsx
@@ -0,0 +1,35 @@
+import styles from "./CardPersonagem.module.css"
+
+function CardPersonagem({ personagem, idioma }) {
+    return (
+        <div className={styles.card}>
+            <img
+            src={personagem.imagem_url}
+            alt={personagem.nome}
+            className={styles.imagem}
+        />
+
+        <div className={styles.overlay}></div>
+        <div className={styles.conteudo}>
+        <h3 className={styles.nome}>
+            {personagem.nome}
+        </h3>
+
+        <p className={styles.papel}>
+            {personagem.papel}
+        </p>
+
+        <p className={styles.descricao}>
+            {
+                idioma === "pt"
+                ? personagem.descricao_pt
+                : personagem.descricao_en
+            }
+        </p>
+        </div>
+
+        </div>
+    )
+}
+
+export default CardPersonagem;
